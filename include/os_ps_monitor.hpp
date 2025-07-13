22247935de2c4c319a14d93a599ddc4d010e8c04
ayhamkhres17 <158040767+ayhamkhres17@users.noreply.github.com>
2025-07-13 17:53:04 +0300

    Update os_ps_monitor.hpp
    after dividing components to different files

--- include/os_ps_monitor.hpp
+++ include/os_ps_monitor.hpp
@@ -1,19 +1,12 @@
 #include <ctime> //time_t
-#include <string> //std::string
-#include <vector> //std::vector
-#include <fstream> //std::ofstream
-#include <sqlite3.h> //sqlite3
 #include <unordered_map>  //std::unordered_map
-#include <set> //std::set
 
+#include "database.hpp"
+#include "logger.hpp"
+#include "configure_info.hpp"
 
-enum Status
-{
-    SUCCESS = 0,
-    FAIL = 1
-};
-
-class ProccesInfo
+ 
+class ProccesInfo //info for each proccess
 {
 public:    
     size_t pid;
@@ -46,88 +39,8 @@ public:
 
 };
 
-class ConfigureInfo //configuration information , responsibility:to load config information from the JSON file
-{
 
-public:
-    explicit ConfigureInfo(const std::string& json_file_path);
-
-    ~ConfigureInfo() noexcept = default;
-
-    ConfigureInfo(const ConfigureInfo& other) = delete;
-    ConfigureInfo& operator=(const ConfigureInfo& other) = delete;
-
-    size_t  GetMonitoringInterval() const { return monitoring_interval; }  
-    size_t  GetMonitoringDuration() const { return monitoring_duration; } //for testing purposes
-    const std::string&  GetDBFilePath() const { return db_file_path; } //for testing purposes
-    const std::string&  GetCSVFilePath() const { return csv_file_path; } //for testing purposes
-    const std::set<size_t>& GetExcludedProcesses() const { return excluded_processes; }  //for testing purposes
-
- 
-private:
-    size_t monitoring_interval;
-    size_t monitoring_duration;
-
-    // std::string dbHost;
-    // std::string dbPort;
-    // std::string dbName;
-    // std::string dbUser;
-    // std::string dbPassword;
-
-    friend class MonitorApp; 
-
-
-    
-    // wer'e using sqlite3 , which does'nt need host,port, name ,user, password we need a filepath
-    std::string db_file_path;
-    std::string csv_file_path;
-
-    std::set<size_t>  excluded_processes; //by pid
-
-};
-
-
-class Database
-{
-public: 
-    explicit Database(const std::string& db_file_path);
-    ~Database() noexcept;
-    
-    Database(const Database& other) = delete;
-    Database& operator=(const Database& other) = delete;
-
-    Status InsertAvgInfo(int monitor_num, double avg_cpu_usage, double avg_memory_usage);
-    sqlite3* GetDb() const { return db; } //for testing
-private:
-    sqlite3 *db;
-
-};
-
-
-class Logger 
-{
-
-public:
-
-    explicit Logger(const std::string& filepath);
-    ~Logger() noexcept;
-
-
-    Logger(const Logger& other) = delete;
-    Logger& operator=(const Logger& other) = delete;
-
-    void write(const std::string str); 
-
-private:
-    std::ofstream target_file;
-    
-};
-
-
-
-
-
-class MonitorApp
+class MonitorApp //app
 {
 public:
     explicit MonitorApp(const ConfigureInfo& config_info);
